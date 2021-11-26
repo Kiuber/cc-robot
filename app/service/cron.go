edd0a25fd944d4826a4dedee9a37584dd2923cfe
kiuber <kiuber.zhang@gmail.com>
2021-11-26 14:22:30 +0800

    update: every twenty for cron

--- app/service/cron.go
+++ app/service/cron.go
@@ -8,13 +8,13 @@ import (
 func RunCron(app *App) {
 	clog.EventLog.Info("run cron")
 	c := ccron.New()
-	ccron.AddFunc(c, "10 * * * *", func() {
+	ccron.AddFunc(c, "*/20 * * * *", func() {
 		app.Exchange.SaveAPISupportSymbolPairsOfAllExchanges()
 	}, true)
-	ccron.AddFunc(c, "10 * * * *", func() {
+	ccron.AddFunc(c, "*/20 * * * *", func() {
 		app.Prime.CheckAndAlarmSymbolPairsOfAllExchanges()
 	}, true)
-	ccron.AddFunc(c, "10 * * * *", func() {
+	ccron.AddFunc(c, "*/20 * * * *", func() {
 		app.Prime.TryUpdatePrimeSymbolPair()
 	}, true)
 	c.Start()
